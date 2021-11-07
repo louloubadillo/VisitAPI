2fe089dac636f4a19d460c3f083e7d9c41e6bb03
louloubadillo <lulu.badillo.csm@gmail.com>
2021-11-06 19:55:22 -0600

    modify trabajadores from website

--- index.js
+++ index.js
@@ -9,16 +9,47 @@ app.use(cors());
 app.use(express.static('static'));
 
 const connection = mysql.createConnection({
-    //rewrite when testing
-    host: "",
-    user: "", 
+    host: "localhost",
+    user: "lulu", 
     password: "", 
-    database: ""
+    database: "visitapp"
 });
 connection.connect();
 
+//Empleados 
+//Agregar empleado a la base de datos
+app.post('/agregar-trabajador', (req, res)=>{
+    data = req.body;
+    // console.log(data);
+    connection.query('INSERT INTO Trabajadores SET ?', data, (err, results, fields)=>{
+      console.log(err)
+      res.send('Se ha añadido al trabajador de manera exitosa');
+    })
+  })
 
+//Modificar 
+app.post('/modificar-trabajador', (req, res)=>{
+    data = req.body;
+    let nombre = data['nombre'];
+    let email = data['email'];
+    let esEncargado = data['encargado']
+    let userID = data['id_trabajador'];
+    connection.query('UPDATE Trabajadores SET ? WHERE id_trabajador='+userID, { nombre, email, esEncargado }, (err, results, fields)=>{
+      console.log(err)
+      res.send('Se ha modificado el trabajador de manera exitosa');
+    })
+    
+  })
 
+//Borrar trabajador
+app.post('/borrar-trabajador', (req, res)=>{
+    let userID = req.body.id_trabajador;
+    connection.query('DELETE FROM Trabajadores WHERE id_trabajador='+userID, (err, results, fields)=>{
+      console.log(err)
+      res.send('Se ha eliminado al trabajador de manera exitosa');
+    })
+  })
+  
 // Regresa la lista de trabajadores
 app.get('/trabajadores', (req, res) => {
     connection.query('SELECT * FROM Trabajadores; ', function (err, results, fields) {
